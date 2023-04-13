c9938b20799829b32bf23c4c8733016cca5d4beb
zehong <zehong.hu@intranda.com>
2023-04-13 13:41:05 +0200

    remove unneeded ServletContextListener

--- plugin/src/main/java/de/intranda/goobi/plugins/hotfolder/nli/HotfolderNLIQuartzJob.java
+++ plugin/src/main/java/de/intranda/goobi/plugins/hotfolder/nli/HotfolderNLIQuartzJob.java
@@ -8,14 +8,10 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
-import java.text.ParseException;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
-
-import javax.servlet.ServletContextEvent;
-import javax.servlet.ServletContextListener;
 
 import org.apache.commons.configuration.SubnodeConfiguration;
 import org.apache.commons.configuration.XMLConfiguration;
@@ -25,14 +21,8 @@ import org.goobi.production.flow.helper.JobCreation;
 import org.goobi.production.flow.jobs.AbstractGoobiJob;
 import org.goobi.production.importer.ImportObject;
 import org.goobi.production.importer.Record;
-import org.quartz.CronTrigger;
-import org.quartz.JobDetail;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
-import org.quartz.Scheduler;
-import org.quartz.SchedulerException;
-import org.quartz.SchedulerFactory;
-import org.quartz.impl.StdSchedulerFactory;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -45,7 +35,7 @@ import de.sub.goobi.persistence.managers.ProcessManager;
 import lombok.extern.log4j.Log4j2;
 
 @Log4j2
-public class HotfolderNLIQuartzJob extends AbstractGoobiJob implements ServletContextListener {
+public class HotfolderNLIQuartzJob extends AbstractGoobiJob {
     private static String title = "intranda_administration_hotfolder_nli";
 
     //Why is this a global property? Should it not be recreated for each HotfolderFolder?
@@ -290,55 +280,4 @@ public class HotfolderNLIQuartzJob extends AbstractGoobiJob implements ServletCo
         return io;
     }
 
-    /**
-     * ServletContextListener
-     */
-    @Override
-    public void contextInitialized(ServletContextEvent sce) {
-        log.info("Starting 'NLI hotfolder' scheduler");
-        try {
-            XMLConfiguration config = ConfigPlugins.getPluginConfig(title);
-            String cronExpression = config.getString("schedule.cronExpresson", "0 /5 * * * ?");
-
-            // get default scheduler
-            SchedulerFactory schedFact = new StdSchedulerFactory();
-            Scheduler sched = schedFact.getScheduler();
-
-            // configure time to start 
-            java.util.Calendar startTime = java.util.Calendar.getInstance();
-            startTime.add(java.util.Calendar.MINUTE, 1);
-
-            // create new job 
-            JobDetail jobDetail = new JobDetail("NLI hotfolder", "Goobi Admin Plugin", HotfolderNLIQuartzJob.class);
-
-            CronTrigger trigger = new CronTrigger();
-            trigger.setName("NLI hotfolder");
-            trigger.setCronExpression(cronExpression);
-
-            //            Trigger trigger = TriggerUtils.makeMinutelyTrigger(5);
-            //            trigger.setName("NLI hotfolder");
-            //            trigger.setStartTime(startTime.getTime());
-
-            // register job and trigger at scheduler
-            sched.scheduleJob(jobDetail, trigger);
-        } catch (SchedulerException | ParseException e) {
-            log.error("Error while executing the scheduler", e);
-        }
-    }
-
-    /**
-     * ServletContextListener
-     */
-    @Override
-    public void contextDestroyed(ServletContextEvent sce) {
-        // stop the Googlebooks Harvester job
-        try {
-            SchedulerFactory schedFact = new StdSchedulerFactory();
-            Scheduler sched = schedFact.getScheduler();
-            sched.deleteJob("NLI hotfolder", "Goobi Admin Plugin");
-            log.info("Scheduler for 'NLI hotfolder' stopped");
-        } catch (SchedulerException e) {
-            log.error("Error while stopping the job", e);
-        }
-    }
 }
