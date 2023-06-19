98557480bff2912adb3cbff9224527e600564e59
zehong <zehong.hu@intranda.com>
2023-06-19 19:48:50 +0200

    add timestamp to runs' logs

--- plugin/src/main/java/de/intranda/goobi/plugins/hotfolder/nli/model/GUIImportResult.java
+++ plugin/src/main/java/de/intranda/goobi/plugins/hotfolder/nli/model/GUIImportResult.java
@@ -1,5 +1,8 @@
 package de.intranda.goobi.plugins.hotfolder.nli.model;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.goobi.production.importer.ImportObject;
 
 import lombok.Data;
@@ -10,9 +13,13 @@ import lombok.NoArgsConstructor;
 public class GUIImportResult {
     private String importFileName;
     private String errorMessage;
+    private String timestamp;
+
+    private static final SimpleDateFormat sdf = new SimpleDateFormat("dd.MMM.yyyy HH:mm:ss");
 
     public GUIImportResult(ImportObject io) {
         this.importFileName = io.getImportFileName();
         this.errorMessage = io.getErrorMessage();
+        this.timestamp = sdf.format(new Date());
     }
 }
